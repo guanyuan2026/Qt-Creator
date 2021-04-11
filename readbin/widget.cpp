8c6296ae1fd5f28199ce49e2a3686df78760900f
Vincent Yua <15633162026@163.com>
2021-04-11 11:04:41 +0800

    实现了bin文件合成功能。增加4个测试用bin文件，另外的test.bin即是工具合成结果。

--- readbin/widget.cpp
+++ readbin/widget.cpp
@@ -76,7 +76,7 @@ void Widget::on_appBt_clicked()
         qDebug("%s",qPrintable(buf));
 
         ui->apppathEdit->setText(buf);
-        ui->appoffsizeEdit->setText(tr("0x00005000"));  //默认偏移量
+        ui->appoffsizeEdit->setText(tr("0x00003000"));  //默认偏移量
     } else {
         QMessageBox::warning(this, tr("Path"), tr("You did not select any file."));
     }
@@ -107,7 +107,7 @@ void Widget::on_paraBt_clicked()
         qDebug("%s",qPrintable(buf));
 
         ui->parapathEdit->setText(buf);
-        ui->paraoffsizeEdit->setText(tr("0x0001F800")); //默认偏移量
+        ui->paraoffsizeEdit->setText(tr("0x0001E800")); //默认偏移量
     } else {
         QMessageBox::warning(this, tr("Path"), tr("You did not select any file."));
     }
@@ -136,15 +136,63 @@ void Widget::on_generateBt_clicked()
             return;
         }
         QDataStream BinFileData(&readfile);  //将刚刚创建的 file 对象的指针传递给一个QDataStream实例 BinFileData
-        char *pBuff = new char[read_file_info.size()];  //要读取bin文件的大小
-        BinFileData.readRawData(pBuff,static_cast<int>(read_file_info.size()));
-        qDebug() << static_cast<int>(read_file_info.size());
+        char *pBuff = new char[120*1024];    //修改为120K大小，以便读取APP.bin时复用。修改为动态分配？
+        BinFileData.readRawData(pBuff,static_cast<int>(read_file_info.size()));//要读取bin文件的大小
+        qDebug() << "IAP.bin size is " << static_cast<int>(read_file_info.size());    //打印文件长度
         QByteArray DataAllArray;
         DataAllArray = QByteArray(pBuff,static_cast<int>(read_file_info.size()));
-
         file.write(DataAllArray);
         readfile.close();
-         file.close();
+
+        int offsize;    //获取偏移量
+        int fillsize;   //填充数据的长度
+        bool status;
+        offsize = ui->appoffsizeEdit->text().toInt(&status,16); // 表示以16进制方式读取字符串
+        fillsize = offsize - read_file_info.size();
+        qDebug("app offsize = %d", offsize);
+        char *pnouseBuff = new char[100*1024];    //和上面一样，直接分配一个大的定长缓存，以便两次填充无用数据时复用。
+        memset(pnouseBuff, 0, fillsize);
+                //注意此处不能使用sizeof（pnouseBuff）,pnouseBuff是一个指针，sizeof为4
+        qDebug() << "the size of iap end to app start is " << fillsize;
+        DataAllArray = QByteArray(pnouseBuff, fillsize);
+        file.write(DataAllArray);
+
+        readfile.setFileName(ui->apppathEdit->text());  //设置文件名
+        read_file_info.setFile(readfile);   //注意这里，虽然变量名称一样，但仍要重新设置才能更新文件信息
+        if (!readfile.open(QIODevice::ReadOnly)) {  //只读方式打开bin文件
+            QMessageBox::warning(this, tr("Read File"),
+                tr("Cannot open file:\n%1").arg(ui->iappathEdit->text()));
+            return;
+        }
+        BinFileData.readRawData(pBuff,static_cast<int>(read_file_info.size()));
+        qDebug() << "APP.bin size is " << static_cast<int>(read_file_info.size());    //打印文件长度
+        DataAllArray = QByteArray(pBuff,static_cast<int>(read_file_info.size()));
+        file.write(DataAllArray);
+        readfile.close();
+
+        offsize = ui->paraoffsizeEdit->text().toInt(&status,16); // 表示以16进制方式读取字符串
+        qDebug("para offsize = %d", offsize);
+        fillsize = offsize - ui->appoffsizeEdit->text().toInt(&status,16) - read_file_info.size();
+        memset(pnouseBuff, 0xFF, fillsize);
+                //注意此处不能使用sizeof（pnouseBuff）,pnouseBuff是一个指针，sizeof为4
+        qDebug() << "the size of app end to para start is " << fillsize;
+        DataAllArray = QByteArray(pnouseBuff, fillsize);
+        file.write(DataAllArray);
+
+        readfile.setFileName(ui->parapathEdit->text()); //设置文件名
+        read_file_info.setFile(readfile);   //注意这里，虽然变量名称一样，但仍要重新设置才能更新文件信息
+        if (!readfile.open(QIODevice::ReadOnly)) {  //只读方式打开bin文件
+            QMessageBox::warning(this, tr("Read File"),
+                tr("Cannot open file:\n%1").arg(ui->parapathEdit->text()));
+            return;
+        }
+        BinFileData.readRawData(pBuff,static_cast<int>(read_file_info.size()));
+        qDebug() << "PARA.bin size is " << static_cast<int>(read_file_info.size());    //打印文件长度
+        DataAllArray = QByteArray(pBuff,static_cast<int>(read_file_info.size()));
+        file.write(DataAllArray);
+        readfile.close();
+
+        file.close();
     } else {
         QMessageBox::warning(this, tr("Path"), tr("You did not select any file."));
     }
